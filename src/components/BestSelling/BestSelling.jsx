c4184af6160d57d7f484692636683ffae7e88a31
moazelgandy2 <moazhazem2005@icloud.com>
2024-02-28 12:14:56 +0200

    Add BestSelling component with new images and ratings

--- src/components/BestSelling/BestSelling.jsx
+++ src/components/BestSelling/BestSelling.jsx
@@ -8,6 +8,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { FaStar } from "react-icons/fa";
+
 import { Badge } from "../ui/badge";
 
 function BestSelling() {
@@ -19,9 +21,18 @@ function BestSelling() {
             <CarouselContent>
               <CarouselItem className="basis-1/5">
                 <Card className="flex relative flex-col justify-center items-center text-center py-2">
-                  <CardContent className="h-[250px] bg-[url('https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg')] bg-contain bg-no-repeat bg-center w-full"></CardContent>
-                  <CardHeader>
-                    <CardTitle>dhh</CardTitle>
+                  <CardContent className=" h-[150px] bg-[url('https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg')] bg-contain bg-no-repeat bg-center w-3/4"></CardContent>
+                  <CardHeader className="w-full ">
+                    <CardTitle className="flex flex-col gap-2 items-start text-start w-full">
+                      <span className=" font-light text-lg">Jacket</span>
+                      <span className="text-[#DB4444]">$50</span>
+                      <span className="flex text-[#FFAD33] w-full items-center gap-3">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                      </span>
+                    </CardTitle>
                     <Badge className="absolute top-0 end-2" variant="destructive">
                       Out of stock
                     </Badge>
@@ -30,9 +41,18 @@ function BestSelling() {
               </CarouselItem>
               <CarouselItem className="basis-1/5">
                 <Card className="flex relative flex-col justify-center items-center text-center py-2">
-                  <CardContent className="h-[250px] bg-[url('https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg')] bg-contain bg-no-repeat bg-center w-full"></CardContent>
-                  <CardHeader>
-                    <CardTitle>Women Jacket</CardTitle>
+                  <CardContent className=" h-[150px] bg-[url('https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg')] bg-contain bg-no-repeat bg-center w-3/4"></CardContent>
+                  <CardHeader className="w-full ">
+                    <CardTitle className="flex flex-col gap-2 items-start text-start w-full">
+                      <span className=" font-light text-lg">Jacket</span>
+                      <span className="text-[#DB4444]">$50</span>
+                      <span className="flex text-[#FFAD33] w-full items-center gap-3">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                      </span>
+                    </CardTitle>
                     <Badge className="absolute top-0 end-2" variant="destructive">
                       Out of stock
                     </Badge>
