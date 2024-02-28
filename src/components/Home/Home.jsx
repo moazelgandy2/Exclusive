7dfc9078f8e464de6791b3ad65861d4a473cf02f
moazelgandy2 <moazhazem2005@icloud.com>
2024-02-28 18:14:19 +0200

    Add NewArrival component to Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../Header/Header";
 import Featured from "../Featured/Featured";
 import Categories from "@/Contexts/Categories/Categories";
 import BestSelling from "../BestSelling/BestSelling";
+import NewArrival from "../NewArrival/NewArrival";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Featured />
       <Categories />
       <BestSelling />
+      <NewArrival />
     </>
   );
 }
---
+++ src/components/NewArrival/NewArrival.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewArrival() {
+  return <div>NewArrival</div>;
+}
+
+export default NewArrival;
