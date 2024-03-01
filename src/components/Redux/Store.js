329b553471445922c69ddf6eb178d75dd2a5a647
moazelgandy2 <moazhazem2005@icloud.com>
2024-03-01 20:59:53 +0200

    Removed unnecessary components.

--- src/components/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productsRed } from "./ProdcutsSlice";
-
-const store = configureStore({
-  reducer: {
-    productsRed: productsRed,
-  },
-});
