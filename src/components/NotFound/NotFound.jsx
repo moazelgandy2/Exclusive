7dd2564e4a91ca7d1cbf1e8ed8e10bb694d66c13
moazelgandy2 <moazhazem2005@icloud.com>
2024-03-02 22:40:51 +0200

    Add Helmet component to NotFound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <>
+      <Helmet>
+        <title>NotFound</title>
+        <meta name="description" content={`NotFound page`} />
+      </Helmet>
       <div className="min-h-[100vh] text-center flex flex-col gap-5 justify-center items-center">
         <h2 className="text-5xl font-bold">404 Not Found</h2>
         <p>You have visited a page that does not exist. You may return to the home page.</p>
